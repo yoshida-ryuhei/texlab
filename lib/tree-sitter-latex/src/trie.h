b2b8dd6f781c446f0c3a119b8d8bfda69c5f1d7a
Patrick Förster <pfoerster@users.noreply.github.com>
2021-12-19 20:59:36 +0100

    Fix compile warnings for tree-sitter-latex scanner

--- lib/tree-sitter-latex/src/trie.h
+++ lib/tree-sitter-latex/src/trie.h
@@ -5,48 +5,48 @@
 
 #define ALPHABET_SIZE 128
 
-typedef struct {
-  struct trie *children[ALPHABET_SIZE];
-  int value;
+typedef struct trie {
+    struct trie *children[ALPHABET_SIZE];
+    int value;
 } trie;
 
 static trie *trie_new() {
-  trie *root = (struct trie *)malloc(sizeof(trie));
-  root->value = 0;
-  memset(root->children, NULL, sizeof(trie *) * ALPHABET_SIZE);
-  return root;
+    trie *root = (trie *)malloc(sizeof(trie));
+    root->value = 0;
+    memset(root->children, 0, sizeof(trie *) * ALPHABET_SIZE);
+    return root;
 }
 
 static void trie_insert(trie *root, char *key, int value) {
-  trie *node = root;
-  for (int i = 0; key[i]; i++) {
-    char c = key[i];
-    if (!node->children[c]) {
-      node->children[c] = trie_new();
+    trie *node = root;
+    for (int i = 0; key[i]; i++) {
+        int c = (int)key[i];
+        if (!node->children[c]) {
+            node->children[c] = trie_new();
+        }
+
+        node = node->children[c];
     }
 
-    node = node->children[c];
-  }
-
-  node->value = value;
+    node->value = value;
 }
 
 static void trie_insert_group(trie *root, char **keys, int value) {
-  for (int i = 0; keys[i]; i++) {
-    trie_insert(root, keys[i], value);
-  }
+    for (int i = 0; keys[i]; i++) {
+        trie_insert(root, keys[i], value);
+    }
 }
 
 static trie *trie_lookup(trie *root, int32_t c) {
-  return root && (c < ALPHABET_SIZE) ? root->children[c] : NULL;
+    return root && (c < ALPHABET_SIZE) ? root->children[c] : NULL;
 }
 
 static void trie_free(trie *root) {
-  for (int i = 0; i < ALPHABET_SIZE; i++) {
-    if (root->children[i]) {
-      trie_free(root->children[i]);
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        if (root->children[i]) {
+            trie_free(root->children[i]);
+        }
     }
-  }
 
-  free(root);
+    free(root);
 }
